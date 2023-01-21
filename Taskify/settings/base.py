6fac91d6161a848e7860e2bdfeef40fce0bd2428
Sreeshanth8 <sreeshanththekkedath8@gmail.com>
2023-01-21 18:10:03 +0530

    permission Taskify Auth

--- Taskify/settings/base.py
+++ Taskify/settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
+    'rest_framework.authtoken',
     
     'Taskify_auth',
 ]
@@ -128,3 +129,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#rest framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
--- Taskify_auth/urls.py
+++ Taskify_auth/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path,include
 
 from rest_framework import routers
+from rest_framework.authtoken import views
 
-from Taskify_auth.views import UserViewSet
+from Taskify_auth.views import UserViewSet, CustomAuthToken, SignIn, GroupsView
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 urlpatterns =[
     path('',include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
+    path('sign-in/', CustomAuthToken.as_view()),
+    path('sign-in-session/', SignIn.as_view()),
+    path('roles/', GroupsView.as_view()),
 ]
