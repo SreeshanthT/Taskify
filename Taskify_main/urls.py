a993cefe52644d8f064ca75ffa64bfacf0f64a36
Sreeshanth T <sreeshanth@reizendretail.in>
2023-06-17 16:51:19 +0530

    # project url edit path added

--- Taskify_main/urls.py
+++ Taskify_main/urls.py
@@ -4,7 +4,8 @@ from Taskify_main.views.projects import ProjectListView, ProjectManageView
 app_name = 'main'
 urlpatterns_projects = [
     path("", ProjectListView.as_view(), name="project_list"),
-    path("create/", ProjectManageView.as_view(method_name="manage"), name="project_add")
+    path("create/", ProjectManageView.as_view(method_name="manage"), name="project_add"),
+    path("edit/<str:pk>/", ProjectManageView.as_view(method_name="manage"), name="project_edit")
 ]
 
 
